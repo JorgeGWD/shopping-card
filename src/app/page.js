ba68a818a1a9a56300906ec09f125900473eac99
JorgeGWD <jorge.rodriguez.r@gmail.com>
2024-01-30 17:45:07 -0500

    The pages.js file was cleaned and the Products file was imported from the components folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+import Products from "./components/Products"
+import { data } from "./services/data"
 
 export default function Home() {
+    const {products} = data
+
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+            {products.map((product) => (
+                <Products key={product.id} product={product} />
+            ))}
         </div>
-    );
+    )
 }
